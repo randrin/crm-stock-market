0489cc1993e8bb15f0b4e311625dde16b6443cb6
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2021-12-04 20:59:26 +0100

    Create entities classes

---
+++ src/main/java/com/crm/market/stock/model/LigneVente.java
@@ -0,0 +1,17 @@
+package com.crm.market.stock.model;
+
+import com.crm.market.stock.model.common.AbstractEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class LigneVente extends AbstractEntity {
+}
--- src/main/java/com/crm/market/stock/model/common/AbstractEntity.java
+++ src/main/java/com/crm/market/stock/model/common/AbstractEntity.java
@@ -4,16 +4,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractEntity implements Serializable {
 
     @Id
